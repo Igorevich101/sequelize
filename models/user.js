a939c1332d8837fd34e2ce2cbaeb8ea6c8b79737
igorevich101 <igorevich101@gmail.com>
2024-09-27 07:56:39 +0300

    Refactor User Model

--- models/user.js
+++ models/user.js
@@ -14,16 +14,64 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.TEXT,
-    isMale: DataTypes.BOOLEAN,
-    birthday: DataTypes.DATEONLY,
-    balance: DataTypes.DECIMAL
+    firstName: { 
+      type: DataTypes.STRING(128),
+      field: 'first_name',
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    lastName: { 
+      type: DataTypes.STRING(128),
+      field: 'last_name',
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    email: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        isEmail: true
+      } 
+    },
+    password: { 
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        notNull: true
+      }
+    },
+    isMale: { 
+      type: DataTypes.BOOLEAN,
+      field: 'is_male'
+    },
+    birthday: { 
+      type: DataTypes.DATEONLY
+    },
+    balance: { 
+      type: DataTypes.DECIMAL(9,2),
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        notNull: true,
+        isNumeric: true,
+        min:0
+      } 
+    }
   }, {
     sequelize,
     modelName: 'User',
+    underscored: true,
+    tableName: 'users'
   });
   return User;
 };
