75a0869ca2e394a3562c05943b5a7ab5f42102d2
igorevich101 <igorevich101@gmail.com>
2024-10-17 09:59:13 +0300

    Add rest Todo CRUD routes

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -48,4 +48,50 @@ module.exports.getUserTodos = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+module.exports.getTodo = async (req, res, next) => {
+  try {
+    const { params : {todoId, userId}} = req;
+
+    const todo = await Todo.findByPk(todoId);
+
+    res.send({ data: todo});
+  } catch (error) {
+    next(error);
+  }
 }
+
+module.exports.updateTodo = async (req, res, next) => {
+  try {
+    const { params: {todoId},
+    body,} =req;
+    
+    const [rowsCount, [updateTodo]] = await Todo.update(body, {
+      where: {
+        id: todoId
+      },
+      returning: true
+    });
+
+    res.send({ data: updateTodo});
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteTodo = async (req, res, next) => {
+  try {
+    const {
+      params: { todoId },
+    } = req;
+
+    const todoToDelete = await Todo.findByPk(todoId);
+
+    await todoToDelete.destroy();
+
+    res.send({ data: todoToDelete });
+  } catch (error) {
+    next(error);
+  }
+};
